0c39416c3520790a14840b148d8fb4a51e2d89d8
AKaniru Victory <vic3coorp@gmail.com>
2019-08-29 20:19:31 +0100

    add bootstrap styling

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 import os
 
 app = Flask(__name__)
@@ -16,6 +17,7 @@ login = LoginManager(app)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
